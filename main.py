cc2fbf13ba0fcdc4fe5beb425e1c7f29de7cdf73
The Khai <c0a221537d@edu.teu.ac.jp>
2023-12-19 15:02:23 +0900

    初期状態

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import pygame as pg
 import sys
 from screen import *
+from ship import *
+import os
 WIDTH, HEIGHT = 1400, 600
+MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
 
 def main():
-    global WIDTH, HEIGHT
-    # 背景インスタンスの作成
-    screen = Screen("battleship", WIDTH, HEIGHT, "/Users/patrickdharma/Desktop/プロエン/ProjExD2023/battleship/imgs/bg_ocean.png")
-    screen.set_screen()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load(f"{MAIN_DIR}/imgs/bg_ocean.png")
     # クロックの作成
-    clock = pg.time.Clock()
+
+    ship1 = Ship(1, (100, 200))
+    ship2 = Ship(7, (1000, 500))
+
     tmr = 0
-    pg.display.update()
-    
+    clock = pg.time.Clock()
+
     while True:
         # イベントハンドラー
+        key_lst = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        
+
         # 背景をブリット
-        screen.blit_screen()
+        screen.blit(bg_img, [0, 0])
+
+        ship1.update(key_lst, screen)
+        ship2.update(key_lst, screen)
 
         pg.display.update()
         tmr += 1
@@ -29,6 +37,7 @@ def main():
 
 
 if __name__ == "__main__":
+
     pg.init()
     main()
     pg.quit()
---
+++ ship.py
@@ -0,0 +1,45 @@
+import pygame as pg
+import sys
+import os
+MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
+
+
+class Ship(pg.sprite.Sprite):
+    """
+    船作成
+    """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_LEFT: (-1, 0),
+        pg.K_RIGHT: (+1, 0),
+    }
+
+    def __init__(self, num: int, xy: tuple[int, int]):
+        super().__init__()
+        img0 = pg.transform.rotozoom(pg.image.load(
+            f"{MAIN_DIR}/fig/1.png"), 0, 2.0)
+        img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん
+        self.imgs = {
+            (+1, 0): img,  # 右
+            (-1, 0): img0,  # 左
+        }
+
+        self.dire = (+1, 0)
+        self.image = self.imgs[self.dire]
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = xy
+        self.speed = 10
+
+    def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
+        sum_mv = [0, 0]
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                self.rect.move_ip(+self.speed*mv[0], +self.speed*mv[1])
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        screen.blit(self.image, self.rect)
