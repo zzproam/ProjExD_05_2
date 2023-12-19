1d9468f24ef4601cd6af3b547d1f5276f49bbeb1
C0B22059 <c0b2205907@edu.teu.ac.jp>
2023-12-19 17:58:51 +0900

    もろもろ修正 by fsm

---
+++ main.py
@@ -0,0 +1,78 @@
+import pygame as pg
+import sys
+from screen import *
+from ship import *
+import os
+WIDTH, HEIGHT = 1400, 600
+MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
+from pygame.locals import *
+
+
+class HealthBar():             #ヘルスバークラス
+    def __init__(self,x,y,width,max):
+        self.x = x
+        self.y = y
+        self.width = width 
+        self.max = max #最大HP
+        self.hp = max
+        self.mark = int((self.width-4)/self.max) #HPバーの1メモリ
+
+        self.font = pg.font.Font(None, 32)
+        self.label = self.font.render("HP",True,(255,255,255))
+        self.frame = Rect(self.x + 2 + self.label.get_width(),self.y + self.width,self.label.get_height())   #ヘルスバーのまわり
+        self.bar = Rect(self.x + 4 + self.label.get_width(),self.y + 2,self.width -4, self.label.get_height()-4)  #ヘルスバー自体
+        self.value = Rect(self.x + 4 + self.label.get_width(),self.y + 2, self.width -4, self.label.get_height() -4)  #ヘルスバーの減ったところ
+
+    def draw(self,screen):
+        pg.draw.rect(screen,(255,255,255),self.frame)
+        pg.draw.rect(screen,(0,0,0),self.bar)
+        pg.draw.rect(screen, (0,255,0),self.value)
+        screen.blit(self.label,(self.x,self.y))
+
+    
+
+
+def main():
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load(f"{MAIN_DIR}/imgs/bg_ocean.png")
+    # クロックの作成
+    hp_bar = HealthBar(10,10,100,300)
+
+    ship1 = Ship(1, (100, 200))
+    ship2 = Ship(7, (1000, 500))
+    ship1_controls = {
+        pg.K_LEFT: (-1, 0),
+        pg.K_RIGHT: (1, 0),
+    }
+    ship2_controls = {
+        pg.K_a: (-1, 0),
+        pg.K_d: (1, 0),
+    }
+
+    tmr = 0
+    clock = pg.time.Clock()
+
+    while True:
+        # イベントハンドラー
+        key_lst = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        # 背景をブリット
+        screen.blit(bg_img, [0, 0])
+        hp_bar.draw(screen)
+        ship1.update(key_lst, ship1_controls, screen)
+        ship2.update(key_lst, ship2_controls, screen)
+
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+
+
+if __name__ == "__main__":
+
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
--- ship.py
+++ ship.py
@@ -8,15 +8,11 @@ class Ship(pg.sprite.Sprite):
     """
     船作成
     """
-    delta = {  # 押下キーと移動量の辞書
-        pg.K_LEFT: (-1, 0),
-        pg.K_RIGHT: (+1, 0),
-    }
 
     def __init__(self, num: int, xy: tuple[int, int]):
         super().__init__()
         img0 = pg.transform.rotozoom(pg.image.load(
-            f"{MAIN_DIR}/fig/1.png"), 0, 2.0)
+            f"{MAIN_DIR}/fig/{num}.png"), 0, 1.0)
         img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん
         self.imgs = {
             (+1, 0): img,  # 右
@@ -30,16 +26,17 @@ class Ship(pg.sprite.Sprite):
         self.rect.center = xy
         self.speed = 10
 
-    def update(self, key_lst: list[bool], screen: pg.Surface):
+    def update(self, key_lst: list[bool], ctrl_keys: dict, screen: pg.Surface):
         """
         押下キーに応じてこうかとんを移動させる
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
         sum_mv = [0, 0]
-        for k, mv in __class__.delta.items():
+        for k, mv in ctrl_keys.items():
             if key_lst[k]:
                 self.rect.move_ip(+self.speed*mv[0], +self.speed*mv[1])
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         screen.blit(self.image, self.rect)
+
