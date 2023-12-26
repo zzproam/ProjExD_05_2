652699cf544b00d7ce86d68104a8e3d7d72de5e2
C0B22059 <c0b2205907@edu.teu.ac.jp>
2023-12-26 15:28:15 +0900

    ヘルスバー

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from screen import *
 from ship import *
 import os
 import random
+from pygame.locals import *
 WIDTH, HEIGHT = 1600, 900
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
@@ -308,6 +309,52 @@ class AnimatedShield(pg.sprite.Sprite):
         self.animate()
         self.rect = self.image.get_rect(center=self.ship.rect.center)
 
+class HealthBar():             #ヘルスバークラス
+    def __init__(self,x,y,width,max):
+        self.x = x
+        self.y = y
+        self.width = width 
+        self.max = max #最大HP
+        self.hp = max
+        self.mark = int((self.width-4)/self.max) #HPバーの1メモリ
+
+        self.font = pg.font.Font(None, 32)
+        self.label = self.font.render("HP",True,(255,255,255))
+        self.frame = Rect(self.x + 2 + self.label.get_width(),self.y, self.width,self.label.get_height())   #ヘルスバーのまわり
+        self.bar = Rect(self.x + 4 + self.label.get_width(),self.y + 2,self.width -4, self.label.get_height() -4)  #ヘルスバー自体
+        self.value = Rect(self.x + 4 + self.label.get_width(),self.y + 2, self.width -4, self.label.get_height() -4)  #ヘルスバーの減ったところ
+
+        self.effect_bar = Rect(self.x + 4 + self.label.get_width(), self.y + 2, self.width - 4, self.label.get_height() -4)
+        self.effect_color = (0,255,255)
+
+    def update(self):
+        if self.hp >= self.max:
+            self.hp = self.max
+
+        if self.effect_bar.width > self.mark * self.hp:
+            self.value.width = self.mark * self.hp
+            if self.effect_bar.width >= self.value.width:
+                self.effect_bar.width = self.mark * self.hp
+
+        elif self.value.width < self.mark * self.hp:
+            self.effect_bar.width = self.mark * self.hp
+            self.value.inflate_ip(1,0)
+
+        if self.effect_bar.width <= self.bar.width /6:
+            self.effect_color - (255,255,0)
+        elif self.effect_color <= self.bat.width /2:
+            self.effect_color = (255,255,0)
+        else:
+            self.effct_color = (0,255,0)
+
+    
+    def draw(self,screen):
+        pg.draw.rect(screen,(255,255,255),self.frame)
+        pg.draw.rect(screen,(0,0,0),self.bar)
+        pg.draw.rect(screen,self.effect_color,self.effect_bar)
+        pg.draw.rect(screen, (0,0,255),self.value)
+        screen.blit(self.label,(self.x,self.y))
+
 
 def main():
 
@@ -317,6 +364,7 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
 
     bg_img_original = pg.image.load(f"{MAIN_DIR}/imgs/bg_ocean.png")
+    hp_bar = HealthBar(10,10,100,300)
     bg_img = pg.transform.scale(bg_img_original, (WIDTH, HEIGHT))
     bg_img_flipped = pg.transform.flip(bg_img, True, False)
     bg_x = 0
@@ -413,6 +461,7 @@ def main():
             bg_x_flipped = bg_img.get_width()
         screen.blit(bg_img, (bg_x, 0))
         screen.blit(bg_img_flipped, (bg_x_flipped, 0))
+        hp_bar.draw(screen)
 
         if key_lst[pg.K_LSHIFT]:
             direction = (-1, 0) if key_lst[pg.K_a] else (1, 0)
