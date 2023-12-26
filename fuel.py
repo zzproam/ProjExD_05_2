36f1f792cdec437f51abae5485f4cd1ab38dee26
Haruto <c0b2210735@edu.teu.ac.jp>
2023-12-26 14:11:36 +0900

    追加機能4完了

--- fuel.py
+++ fuel.py
@@ -1,16 +1,14 @@
+#追加機能4
 import pygame as pg
 import random
 import os
+#画面サイズ
 WIDTH, HEIGHT = 1400, 600
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
-
 
 class Fuel(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/1.png"))
+        self.image = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/imgs/123.jpg"), 0, 0.08)
         self.rect = self.image.get_rect()
-        self.rect.center = random.randint(WIDTH, 200), 0 #燃料の生成
-        self.vy = +6
-
-    #def update(self):
+        self.rect.center = random.randint(0, WIDTH), random.choice([200,500])
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame as pg
 import sys
 from screen import *
 from ship import *
+from fuel import *
+from score import *
 import os
 WIDTH, HEIGHT = 1400, 600
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
@@ -11,7 +13,10 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load(f"{MAIN_DIR}/imgs/bg_ocean.png")
     # クロックの作成
-
+    #scoreの表示
+    score = Score()
+    score2 = Scores()
+    fuels = pg.sprite.Group()
     ship1 = Ship(1, (100, 200))
     ship2 = Ship(7, (1000, 500))
     ship1_controls = {
@@ -30,15 +35,29 @@ def main():
         # イベントハンドラー
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
-
+             if event.type == pg.QUIT:
+                 return
+             
+        #燃料と船が接触したかの判定
+        for fuel in fuels:
+            if ship1.rect.colliderect(fuel.rect):
+                score.score += 20
+                fuel.kill()
+            if ship2.rect.colliderect(fuel.rect):
+                score2.scores += 20
+                fuel.kill()
+        
         # 背景をブリット
         screen.blit(bg_img, [0, 0])
+        # # 300フレームに1回，燃料を出現させる
+        if tmr % 4 == 0:
+            fuels.add(Fuel())
 
         ship1.update(key_lst, ship1_controls, screen)
         ship2.update(key_lst, ship2_controls, screen)
-
+        score.update(screen)
+        score2.update(screen)
+        fuels.draw(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
